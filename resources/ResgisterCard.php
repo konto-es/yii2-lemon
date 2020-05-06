b439271cc1a5ef4fc15d4ca0c7688e7afcbf24f2
yusney <ymartinez@sicma21.com>
2020-05-06 22:46:45 +0200

    model where the cards are registered

---
+++ resources/ResgisterCard.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace kowi\lemon\resources;
+
+
+use kowi\lemon\objects\Card;
+
+class ResgisterCard extends LemonwayResource
+{
+    public $id;
+    /**
+     * @var string $accountId string [ 0 .. 256 ] characters
+     */
+    public $accountId;
+
+    /**
+     * @var Card $card
+     */
+    public $card;
+
+    public function rules()
+    {
+        return array_merge(parent::rules(), [
+            /** @see https://apidoc.lemonway.com/#operation/MoneyIns_CardRegisterPost **/
+            [['accountId', 'card'], 'required', 'on' => [static::SCENARIO_CREATE]],
+        ]);
+    }
+
+    public static function resource()
+    {
+        return [
+            static::SCENARIO_LOAD => '/v2/moneyins/{accountid}/card',
+            static::SCENARIO_CREATE => '/v2/moneyins/card/register',
+           // static::SCENARIO_UPDATE => '/v2/accounts/individual',
+        ];
+    }
+
+}
